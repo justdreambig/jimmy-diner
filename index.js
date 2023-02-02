a2d5513704e7dc5d9ff44660bc381f6edf18c316
justdreambig <39072111+justdreambig@users.noreply.github.com>
2023-02-01 22:06:24 -0800

    added more functions
    remember to hang in there and just keep swimming

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { menuArray } from './data.js'
-const completeOrder = document.getElementById("complete-order");
+// const completeOrder = document.getElementById("complete-order");
 
 // Building the menu from a list of items in menuArray from data.js
 const menuList = document.getElementById('menu');
@@ -37,54 +37,79 @@ menuList.innerHTML = getMenuHtml();
 // Order Screen //
 
 let addedOrderItems = []
-let addedOrderItemsTotal =[]
 
-document.addEventListener('click', function(e){
+    document.addEventListener('click', function(e){
      
-    if (e.target.dataset.addItemsId) {
-        handleAddItem(e.target.dataset.addItemsId)
-        toggleHiddenOrder()
-        
-    } else if (e.target.dataset.remove){
-
-    }
-
-    })
-
-
-
-        function handleAddItem(menuId){
-        const targetMenuObj = menuArray.filter(function(menu){
-        return Number(menu.id) === Number(menuId)
-        })[0]
-        
-        addedOrderItems.push(targetMenuObj)
-        getOrderHtml()
-    }
-    
-    function getOrderHtml() {
-        const orderList = document.getElementById('added-order-items');
-        let orderHtml = ``
-
-        // for(let i = 0; i < arr.length; i++) 
-        addedOrderItems.forEach(function(order) {
+        if (e.target.dataset.addItemsId) {
+            handleAddItem(e.target.dataset.addItemsId)
             
-            orderHtml += `
-                <div class="menu-item-description order-item flex">   
-                    <div class="menu-item-name order-name"> ${order.name} </div>
-                    <div class="menu-item-name remove" data-remove="${order.id}"> remove </div>
-                    <div class="menu-item-price order-price">$${order.price}</div>
-                </div>
-            `; 
-            
+        } else if (e.target.dataset.remove){
+            removeOrderItem (e.target.dataset.remove)
+            }
         })
+            function handleAddItem(menuId){
+                const targetMenuObj = menuArray.filter(function(menu){
+                return Number(menu.id) === Number(menuId)
+                })[0]
+            
+                addedOrderItems.push(targetMenuObj)
+                getOrderHtml()
+            }
 
-        if (document.getElementById('order').classList == ('hidden')){
-            document.getElementById('order').classList.toggle('hidden');
-        }
+            function removeOrderItem (orderItemId) {
+                for (let i = 0; i < addedOrderItems.length; i++) {
+                    if (Number(addedOrderItems[i].id) === Number(orderItemId)) {
+                        addedOrderItems.splice(i, 1);
+                        break;
+                    }
+                }
+                getOrderHtml()
+            }
 
-        orderList.innerHTML = orderHtml;  
+        function getOrderHtml() {
+            const orderList = document.getElementById('added-order-items');
+            let orderHtml = ``
+
+            // for(let i = 0; i < arr.length; i++) 
+            addedOrderItems.forEach(function(order) {
+                
+                orderHtml += `
+                    <div class="menu-item-description order-item flex">   
+                        <div class="menu-item-name order-name"> ${order.name} </div>
+                        <div class="menu-item-name remove" data-remove="${order.id}"> remove </div>
+                        <div class="menu-item-price order-price">$${order.price}</div>
+                    </div>
+                `; 
+                
+            })
+
+            orderList.innerHTML = orderHtml;
+            getOrderTotalPrice()
+            hideOrder ()  
+    }
+
+    function getOrderTotalPrice() {
+        const totalPrice = addedOrderItems.reduce((sum, item) => sum + item.price, 0);
+        const totalElement = document.getElementById("total");
+        totalElement.innerHTML = totalPrice;
         
-}
+    }
 
-console.log("Let's Go")
+    // Toggle between hidden and shown if order has items or not 
+
+        function hideOrder () {
+            
+            if(addedOrderItems.length > 0 ) {
+                let order = (document.getElementById('order'))
+                    if (order.classList.contains('hidden')) 
+                        order.classList.remove('hidden')
+                        
+                    } else { 
+                        order.classList.add('hidden')
+                        
+                    }
+                }
+                
+
+console.log("🐠 🐳  Just Keep Swimming!  🐙 🐡 ")
+ 
