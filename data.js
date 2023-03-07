104fdae575b0fe77d4d88c820012fdffef60a112
justdreambig <39072111+justdreambig@users.noreply.github.com>
2023-03-06 17:58:11 -0800

    updated Format

--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 export const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni",  "mushrom",  "mozarella"],
-        price: 14,
-        emoji: "🍕",
-        id: 4954,
-        class: "font-red"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef",  "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 4955,
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain,  hops,  yeast,  water"],
-        price: 12,
-        emoji: "🍺",
-        id: 4956,
-    }
-]
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", " ", "mushrom", " ", "mozarella"],
+    price: 14,
+    emoji: "🍕",
+    id: 4954,
+    class: "font-red",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", " ", "cheese", " ", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 4955,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain,  hops,  yeast,  water"],
+    price: 12,
+    emoji: "🍺",
+    id: 4956,
+  },
+];
